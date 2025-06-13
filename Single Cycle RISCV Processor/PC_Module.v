24d802d0e5beec8520a21e3f26a367aff15d54b7
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-13 20:24:23 +0530

    Update PC_Module.v

--- Single Cycle RISCV Processor/PC_Module.v
+++ Single Cycle RISCV Processor/PC_Module.v
@@ -4,11 +4,13 @@ module PC_Module(clk,rst,PC,PC_Next);
     output [31:0]PC;
     reg [31:0]PC;
 
-    always @(posedge clk)
-    begin
-        if(~rst)
-            PC <= {32{1'b0}};
-        else
-            PC <= PC_Next;
-    end
+
+always @(posedge clk or posedge rst)
+begin
+    if(rst)
+        PC <= 32'b0;
+    else
+        PC <= PC_Next;
+end
+
 endmodule
