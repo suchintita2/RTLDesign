a2cb3cb2a33ae8122a7f0429ef35a75dae1e91ab
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-13 20:20:17 +0530

    Update ALU.v

--- Single Cycle RISCV Processor/ALU.v
+++ Single Cycle RISCV Processor/ALU.v
@@ -1,25 +1,26 @@
-module ALU(
-    input wire [31:0] A,
-    input wire [31:0] B,
-    input wire [2:0] ALUControl,
-    output reg [31:0] Result,
-    output wire Zero
-);
-    wire [31:0] sum;
-    wire cout;
+module ALU(A,B,Result,ALUControl,OverFlow,Carry,Zero,Negative);
+
+    input [31:0]A,B;
+    input [2:0]ALUControl;
+    output Carry,OverFlow,Zero,Negative;
+    output [31:0]Result;
+
+    wire Cout;
+    wire [31:0]Sum;
+
+    assign {Cout,Sum} = (ALUControl[0] == 1'b0) ? A + B :
+                                          (A + ((~B)+1)) ;
+    assign Result = (ALUControl == 3'b000) ? Sum :
+                    (ALUControl == 3'b001) ? Sum :
+                    (ALUControl == 3'b010) ? A & B :
+                    (ALUControl == 3'b011) ? A | B :
+                    (ALUControl == 3'b101) ? {{31{1'b0}},(Sum[31])} : {32{1'b0}};
     
-    assign {cout, sum} = (ALUControl[0]) ? A + (~B + 1) : A + B;
-    
-    always @(*) begin
-        case (ALUControl)
-            3'b000: Result = sum;           // ADD
-            3'b001: Result = sum;           // SUB
-            3'b010: Result = A & B;         // AND
-            3'b011: Result = A | B;         // OR
-            3'b101: Result = {{31{1'b0}}, sum[31]}; // SLT
-            default: Result = 32'b0;
-        endcase
-    end
-    
-    assign Zero = (Result == 32'b0);
+    assign OverFlow = ((Sum[31] ^ A[31]) & 
+                      (~(ALUControl[0] ^ B[31] ^ A[31])) &
+                      (~ALUControl[1]));
+    assign Carry = ((~ALUControl[1]) & Cout);
+    assign Zero = &(~Result);
+    assign Negative = Result[31];
+
 endmodule
