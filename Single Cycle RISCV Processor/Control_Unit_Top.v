c93d8f0b37c300308a4d64991d511fe3c79bcfe8
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-13 20:20:48 +0530

    Update Control_Unit_Top.v

--- Single Cycle RISCV Processor/Control_Unit_Top.v
+++ Single Cycle RISCV Processor/Control_Unit_Top.v
@@ -2,24 +2,24 @@
 `include "Main_Decoder.v"
 `include "ALU.v"
 
-module Control_Unit_Top(Op, RegWrite, ImmSrc, ALUSrc, MemWrite, ResultSrc, Branch, PCSrc, funct3, funct7, Zero, ALUControl);
-    input [6:0] Op, funct7;
-    input [2:0] funct3;
-    input Zero;
-    output RegWrite, ALUSrc, MemWrite, Branch, PCSrc;
-    output [1:0] ImmSrc, ResultSrc;
-    output [2:0] ALUControl;
-
+module Control_Unit_Top(
+    input [6:0] Op, funct7,
+    input [2:0] funct3,
+    input Zero,
+    output RegWrite, ALUSrc, MemWrite, Branch, PCSrc,
+    output [1:0] ImmSrc, ResultSrc,
+    output [2:0] ALUControl
+);
     wire [1:0] ALUOp;
 
     Main_Decoder Main_Decoder(
         .Op(Op),
         .RegWrite(RegWrite),
         .ImmSrc(ImmSrc),
+        .ALUSrc(ALUSrc),
         .MemWrite(MemWrite),
         .ResultSrc(ResultSrc),
         .Branch(Branch),
-        .ALUSrc(ALUSrc),
         .ALUOp(ALUOp)
     );
 
@@ -27,9 +27,8 @@ module Control_Unit_Top(Op, RegWrite, ImmSrc, ALUSrc, MemWrite, ResultSrc, Branc
         .ALUOp(ALUOp),
         .funct3(funct3),
         .funct7(funct7),
-        .op(Op),
         .ALUControl(ALUControl)
     );
 
-    assign PCSrc = Branch & Zero;
+    assign PCSrc = (Branch & Zero) | (Op == 7'b1101111);
 endmodule
