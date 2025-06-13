1a700aea4d0fd3407ca79a4da5887cd090a2c1b4
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-13 20:25:24 +0530

    Update Single_Cycle_TB.v

--- Single Cycle RISCV Processor/Single_Cycle_TB.v
+++ Single Cycle RISCV Processor/Single_Cycle_TB.v
@@ -15,7 +15,7 @@ module Single_Cycle_TB;
         clk = 0;
         rst = 1;
         cycle = 0;
-        #15 rst = 0;
+#5 rst = 0;
 
         // Run for fixed time
         #1000;
@@ -24,10 +24,19 @@ module Single_Cycle_TB;
         $finish;
     end
 
-    always @(posedge clk) begin
-        cycle = cycle + 1;
-        $display("Cycle %0d | PC: 0x%h | Instr: 0x%h | Reg[2]: 0x%h",
-                  cycle, uut.PC_Top, uut.RD_Instr, uut.Register_File.Register[2]);
-    end
+always @(posedge clk) begin
+    cycle = cycle + 1;
+$display("Cycle %0d | PC: 0x%h | Instr: 0x%h | x1: 0x%h | x2: 0x%h | x4: 0x%h | x5: 0x%h | x7: 0x%h | x11: 0x%h",
+         cycle, uut.PC_Top, uut.RD_Instr,
+         uut.Register_File.Register[1],
+         uut.Register_File.Register[2],
+         uut.Register_File.Register[4],
+         uut.Register_File.Register[5],
+         uut.Register_File.Register[7],
+         uut.Register_File.Register[11]);    
+$display("PC: 0x%h | Instr: 0x%h | ALUResult: 0x%h | ReadData: 0x%h | Result: 0x%h | RegWrite: %b | ResultSrc: %b | ALUControl: %b",
+         uut.PC_Top, uut.RD_Instr, uut.ALUResult, uut.ReadData, uut.Result, uut.RegWrite, uut.ResultSrc, uut.ALUControl_Top);
 
+end
+        
 endmodule
