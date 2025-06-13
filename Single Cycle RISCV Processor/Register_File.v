dd1ae0c6d1133233b594e38266aefe4baa13f241
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-13 20:24:45 +0530

    Update Register_File.v

--- Single Cycle RISCV Processor/Register_File.v
+++ Single Cycle RISCV Processor/Register_File.v
@@ -1,25 +1,23 @@
-module Register_File(clk,rst,WE3,WD3,A1,A2,A3,RD1,RD2);
-
-    input clk,rst,WE3;
-    input [4:0]A1,A2,A3;
-    input [31:0]WD3;
-    output [31:0]RD1,RD2;
-
+module Register_File(
+    input clk, rst,
+    input WE3,
+    input [4:0] A1, A2, A3,
+    input [31:0] WD3,
+    output [31:0] RD1, RD2
+);
     reg [31:0] Register [31:0];
-
-    always @ (posedge clk)
-    begin
-        if(WE3)
+    
+    assign RD1 = Register[A1];
+    assign RD2 = Register[A2];
+integer i;
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            for(i=0; i<32; i=i+1)
+                Register[i] <= 32'b0;
+        end
+        else if(WE3 && A3 != 5'd0) begin
             Register[A3] <= WD3;
+$display("Write Reg[%0d] <= 0x%h", A3, WD3);
+end
     end
-
-    assign RD1 = (~rst) ? 32'd0 : Register[A1];
-    assign RD2 = (~rst) ? 32'd0 : Register[A2];
-
-    initial begin
-        Register[5] = 32'h00000005;
-        Register[6] = 32'h00000004;
-        
-    end
-
 endmodule
