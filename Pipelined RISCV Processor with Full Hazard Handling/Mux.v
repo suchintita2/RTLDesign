b762534dc5047fb0f884c8257affa9ba632a8333
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-19 16:31:29 +0530

    Delete Pipelined RISCV Processor with Full Hazard Handling directory

--- Pipelined RISCV Processor with Full Hazard Handling/Mux.v
+++
@@ -1,7 +0,0 @@
-module Mux(
-    input [31:0] a, b,
-    input s,
-    output [31:0] c
-);
-    assign c = s ? b : a;
-endmodule
--- Pipelined RISCV Processor with Full Hazard Handling/Mux_3to1.v
+++
@@ -1,9 +0,0 @@
-module Mux_3to1(
-    input [31:0] a, b, c,
-    input [1:0] s,
-    output [31:0] out
-);
-    assign out = (s == 2'b00) ? a :
-                 (s == 2'b01) ? b :
-                 (s == 2'b10) ? c : a;
-endmodule
