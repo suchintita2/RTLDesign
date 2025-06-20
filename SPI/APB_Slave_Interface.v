4cd61d793534702b99721d81b14a228330ec203d
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-21 00:54:29 +0530

    Update and rename abc.v to APB_Slave_Interface.v

--- SPI/APB_Slave_Interface.v
+++ SPI/APB_Slave_Interface.v
@@ -6,21 +6,25 @@ module APB_Slave_Interface(
   output [2:0] sppr, spr, 
   output [7:0] PRDATA);
   
-reg [7:0] SPI_CR1, SPI_CR2, SPI_SR, SPI_DR;
+  reg [7:0] SPI_CR1, SPI_CR2, SPI_SR, SPI_DR, SPI_BR;
+  
 
-wire sptef, spif, spe, modfen, modf, ssoe, wr_enb, rd_enb, spie, sptie;
+  wire sptef, spif, spe, modfen, modf, ssoe, wr_enb, rd_enb, spie, sptie;
 
-parameter cr2_mask = 8'b0001_1011;
-parameter br_mask = 8'b0111_0111;
-parameter spi_run 2'b00;
-parameter spi_wait 2'b01;
-parameter spi_stop 2'b10; 
+  wire SPI_CR1_a, SPI_CR1_b,
+  reg SPI_CR1_c, 
+  
+  parameter cr2_mask = 8'b0001_1011;
+  parameter br_mask = 8'b0111_0111;
+  parameter spi_run 2'b00;
+  parameter spi_wait 2'b01;
+  parameter spi_stop 2'b10; 
 
-reg [1:0] next_mode, STATE, next_state; 
+  reg [1:0] next_mode, STATE, next_state; 
 
-parameter IDLE = 2'600;
-parameter SETUP 2'601; // Setup state
-parameter ENABLE 2'b10; // Enable state
+  parameter IDLE = 2'600;
+  parameter SETUP 2'601; // Setup state
+  parameter ENABLE 2'b10; // Enable state
 
   assign ssoe = SPI_CR1[1];
   assign mstr = SPI_CR1[4];
@@ -42,3 +46,13 @@ parameter ENABLE 2'b10; // Enable state
   assign sptef = (SPI_DR == 8'b0)? 1'b1: 1'b0;
   assign spif = (SPI_DR != 8'b0)? 1'b1: 1'b0;
   assign modf = (~SS) & mstr & modfen & (~ssoe);
+
+  mux m1(.a({spif,1'b0, sptef,modf,4'b0}), .b(8'b00100000), .s(PRESETn), .y(SPI_SR));
+
+  mux spi_cr1_a(.a(SPI_CR1), .b(PWDATA), .s(PADDR == 3'b0), .y(SPI_CR1_a));
+  mux spi_cr1_b(.a(8'b0), .b(SPI_CR1_a), .s(wr_enb), .y(SPI_CR1_b));
+  always@(posedge clk)
+    SPI_CR1_c <= SPI_CR1_b;
+  mux spi_cr1(.a(8'b0), .b(SPI_CR1_c), .s(PRESETn), .y(SPI_CR_1));
+
+  
