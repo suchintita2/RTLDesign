379c2d9405e9133dab7245c08f2f86739e2e321f
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-21 01:40:41 +0530

    Update APB_Slave_Interface.v

--- SPI/APB_Slave_Interface.v
+++ SPI/APB_Slave_Interface.v
@@ -11,8 +11,14 @@ module APB_Slave_Interface(
 
   wire sptef, spif, spe, modfen, modf, ssoe, wr_enb, rd_enb, spie, sptie;
 
-  wire SPI_CR1_a, SPI_CR1_b,
-  reg SPI_CR1_c, 
+  wire [7:0] SPI_CR1_a, SPI_CR1_b, SPI_CR2_a, SPI_CR2_b, SPI_BR_a, SPI_BR_b, PRDATA_a, PRDATA_b, PRDATA_c, PRDATA_d;
+  reg [7:0] SPI_CR1_c, SPI_CR2_c, SPI_BR_c;
+  wire PWDATA_int, select, reuse;
+  wire send_data_a, send_data_b, send_data_c;
+  reg send_data_d;
+  wire [7:0] SPI_DR_a, SPI_DR_b, SPI_DR_c, SPI_DR_d;
+  reg [7:0] SPI_DR_e;
+
   
   parameter cr2_mask = 8'b0001_1011;
   parameter br_mask = 8'b0111_0111;
@@ -47,12 +53,49 @@ module APB_Slave_Interface(
   assign spif = (SPI_DR != 8'b0)? 1'b1: 1'b0;
   assign modf = (~SS) & mstr & modfen & (~ssoe);
 
-  mux m1(.a({spif,1'b0, sptef,modf,4'b0}), .b(8'b00100000), .s(PRESETn), .y(SPI_SR));
+  mux8 m1(.a({spif,1'b0, sptef,modf,4'b0}), .b(8'b00100000), .s(PRESETn), .y(SPI_SR));
 
-  mux spi_cr1_a(.a(SPI_CR1), .b(PWDATA), .s(PADDR == 3'b0), .y(SPI_CR1_a));
-  mux spi_cr1_b(.a(8'b0), .b(SPI_CR1_a), .s(wr_enb), .y(SPI_CR1_b));
-  always@(posedge clk)
+  mux8 spi_cr1_a(.a(SPI_CR1), .b(PWDATA), .s(PADDR == 3'b0), .y(SPI_CR1_a));
+  mux8 spi_cr1_b(.a(8'h00), .b(SPI_CR1_a), .s(wr_enb), .y(SPI_CR1_b));
+  always@(posedge PClk)
     SPI_CR1_c <= SPI_CR1_b;
-  mux spi_cr1(.a(8'b0), .b(SPI_CR1_c), .s(PRESETn), .y(SPI_CR_1));
-
+  mux8 spi_cr1(.a(8'h04), .b(SPI_CR1_c), .s(PRESETn), .y(SPI_CR1));
   
+  mux8 spi_cr2_a(.a(SPI_CR2), .b(PWDATA & cr2_mask), .s(PADDR == 3'b001), .y(SPI_CR2_a));
+  mux8 spi_cr2_b(.a(8'h04), .b(SPI_CR2_a), .s(wr_enb), .y(SPI_CR2_b));
+  always@(posedge PClk)
+    SPI_CR2_c <= SPI_CR2_b;
+  mux8 spi_cr2(.a(8'h00), .b(SPI_CR2_c), .s(PRESETn), .y(SPI_CR2));
+  
+  mux8 spi_br_a(.a(SPI_BR), .b(PWDATA & br_mask), .s(PADDR == 3'b010), .y(SPI_BR_a));
+  mux8 spi_br_b(.a(8'h00), .b(SPI_BR_a), .s(wr_enb), .y(SPI_BR_b));
+  always@(posedge PClk)
+    SPI_BR_c <= SPI_BR_b;
+  mux8 spi_br(.a(8'h00), .b(SPI_BR_c), .s(PRESETn), .y(SPI_BR));
+  
+  mux8 prdata_a(.a(SPI_DR), .b(SPI_SR), .s(PADDR == 3'b011), .y(PRDATA_a));
+  mux8 prdata_b(.a(PRDATA_a), .b(SPI_BR), .s(PADDR == 3'b010), .y(PRDATA_b));
+  mux8 prdata_c(.a(PRDATA_b), .b(SPI_CR2), .s(PADDR == 3'b001), .y(PRDATA_c));
+  mux8 prdata_d(.a(PRDATA_c), .b(SPI_CR1), .s(PADDR == 3'b000), .y(PRDATA_d));
+  mux8 prdata(.a(8'b0), .b(PRDATA_d), .s(rd_enb), .y(PRDATA));
+
+  assign reuse = ((spi_mode == spi_run) | (spi_mode == spi_wait));
+  assign PWDATA_int = ((SPI_DR == PWDATA) & (SPI_DR != miso_data) & reuse);((spi_mode == spi_run) | (spi_mode == spi_wait))
+  assign select = (reuse & receive_data);
+
+  assign send_data_a = select? 1'b0:1'b0;
+  assign send_data_b = PWDATA_int? 1'b1:send_data_a;
+  assign send_data_c = wr_enb? send_data:send_data_b;
+  always@(posedge PClk)
+    send_data_d <= send_data_c;
+  assign send_data = PRESETn? send_data_d:1'b0;
+
+  mux8 spi_dr_a(.a(SPI_DR), .b(miso_data), .s(select), .y(SPI_DR_a));
+  mux8 spi_dr_b(.a(SPI_DR_a), .b(8'b0), .s(PWDATA_int), .y(SPI_DR_b));
+  mux8 spi_dr_c(.a(SPI_DR), .b(PWDATA), .s(PADDR == 3'b101), .y(SPI_DR_c));
+  mux8 spi_dr_d(.a(SPI_DR_b), .b(SPI_DR_c), .s(wr_enb), .y(SPI_DR_c));
+  always@(posedge PClk) 
+    SPI_DR_d <= SPI_DR_c;
+  mux8 spi_dr(.a(8'b0), .b(SPI_DR_d), .s(PRESETn), .y(SPI_DR));
+
+endmodule
