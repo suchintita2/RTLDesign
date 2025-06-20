ae051ddc02acfefde87aa3349f91577d5a1adead
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-21 02:59:07 +0530

    Create flag_low.v

---
+++ SPI/flag_low.v
@@ -0,0 +1,13 @@
+module (input s1, sclk, s2, PCLK, PRESETn, output y);
+
+  wire a,b,c;
+  reg d;
+
+  assign a = s1? 1'b1 : 1'b0;
+  assign b = sclk? 1'b0 : a;
+  assign c = s2? y : b;
+  always@(posedge PClk)
+    d<=c;
+  assign y = PRESETn? d:1'b0;
+
+endmodule
