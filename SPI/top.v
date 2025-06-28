0895cc7c84c5c76e1ba75ec80d45932b274b1e93
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-29 00:45:16 +0530

    Update top.v

--- SPI/top.v
+++ SPI/top.v
@@ -12,17 +12,17 @@ module top(
   wire [11:0] baud_rate_div;
   wire [7:0] data_mosi,data_miso;
   wire [2:0] spr,sppr;
+  
+  Baud_Rate_Generator BAUD_GEN (PCLK, PRESETn, spiswwai, cpol, cpha, ss, spi_mode, sppr, spr, 
+                                sclk, flag_low, flags_low, flag_high, flags_high, baud_rate_div);
 
-  Baud_Rate_Generator BAUD_GEN (PCLK, PRESETn, spi_mode, spiswai, sppr, spr, cpol, cpha, 
-                                ss, sclk, flag_high, flag_low, flags_high, flags_low, baud_rate_div);
-
-  APB_Slave_Interface SLAVE_INTERFACE (PCLK, PRESET, PADDR, PWRITE, PSEL, PENABLE, PWDATA,
-                                       ss, data_miso, receive_data, tip, PRDATA, mstr, cpol, cpha, lsbfe, 
-                                       spiswai, spi_interrupt_request, PREADY, PSLVERR, send_data, data_mosi, spi_mode, sppr, spr);
-
-  spi_slave_control_select SLAVE_SELECT (PCLK, PRESETn, mstr, spiswai, spi_mode, send_data, baud_rate_div, receive_data, ss, tip);
+	APB_Slave_Interface SLAVE_INTERFACE (PCLK, PRESETn, PWRITE, PSEL, PENABLE, ss, receive_data, tip,
+					     PADDR, PWDATA, miso_data, mstr, cpol, cpha, lsbfe, spiswai, spi_interrupt_request, 
+					     PREADY, PSLVERR, send_data, mosi_data, spi_mode, sppr, spr, PRDATA);
+	
+  spi_slave_control_select SLAVE_SELECT (PCLK, PRESETn, mstr, spiswai, send_data, spi_mode, , baud_rate_div, ss, tip, receive_data);
 
   shift_register SHIFT_REG (PCLK, PRESETn, ss, send_data, lsbfe, cpha, cpol, flag_low, flag_high, 
-                            flags_low, flags_high, miso, receive_data, data_mosi, mosi, data_miso);
+                            flags_low, flags_high, miso, receive_data, data_mosi, data_miso, mosi);
 
 endmodule
