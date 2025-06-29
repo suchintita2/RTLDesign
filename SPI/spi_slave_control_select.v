79cbd10d7d679bb092da471e1a1e9b7a2338949c
suchintita2 <ghosh.suchintita2@gmail.com>
2025-06-29 11:32:04 +0530

    Update spi_slave_control_select.v

--- SPI/spi_slave_control_select.v
+++ SPI/spi_slave_control_select.v
@@ -6,34 +6,36 @@ module spi_slave_control_select(
   output reg receive_data);
 
   reg rcv;
-  wire select, enable, target;
+  wire select, enable;
+  wire [15:0] count, target;
 
   assign tip = ~ss;
-  assign target = baud_rate_divisor << 4;
+  
+  assign target = {baud_rate_divisor, 4'b0};  // Multiply by 16 (shift left 4)  
   
   assign select = ((!spiswai) && enable && mstr);
   assign enable = (spi_mode == 2'b00) || (spi_mode == 2'b01);
   
-  always@(posedge clk or negedge PRESETn) begin
+  always@(posedge PCLK or negedge PRESETn) begin
     if(!PRESETn)
       receive_data <= 1'b0;
     else
       receive_data <= rcv;
   end
 
-   always@(posedge clk or negedge PRESETn) begin
+  always@(posedge PCLK or negedge PRESETn) begin
      if(!PRESETn) 
        rcv <= 1'b0;
-     else if (enable && !send_data && (count <= (target - 1'b1)) && (count == (target - 1'b1)))
+    else if (select && !send_data && (count <= (target - 1'b1)) && (count == (target - 1'b1)))
        rcv <= 1'b1;
-     else if (!enable ||  send_data || !(count <= (target - 1'b1)))
+    else if (!select ||  send_data || !(count <= (target - 1'b1)))
        rcv <= 1'b0;
    end
-
-  always@(posedge clk or negedge PRESETn) begin
+    
+  always@(posedge PCLK or negedge PRESETn) begin
      if(!PRESETn) 
        ss <= 1'b1;
-    else if (enable) begin
+    else if (select) begin
       if(!send_data) begin
         if(count <= (target - 1'b1))
           ss <= 1'b0;
@@ -41,6 +43,23 @@ module spi_slave_control_select(
           ss <= 1'b1;
       end else
         ss <= 1'b0;
-    end else if(!enable)
+    end else if(!select)
       ss <= 1'b1;
   end
+    
+  always@(posedge PCLK or negedge PRESETn) begin
+    if(!PRESETn) 
+       count <= 16'hffff;
+    else if (select) begin
+      if(!send_data) begin
+        if(count <= (target - 1'b1))
+          count <= (count + 1'b1);
+        else
+          count <= 16'hffff;
+      end else
+        count <= 16'b0;
+    end else if(!select)
+      count <= 16'hffff;
+  end
+
+endmodule
